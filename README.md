905df926a6d7fb0b08d058bdab230a459b688c75
Andre Sousa <andre.sousa40@etec.sp.gov.br>
2020-08-29 15:59:16 -0300

    Initial README and constants declarations in the main file

---
+++ README.md
@@ -0,0 +1,4 @@
+# My Youtube Recommendation
+
+## About
+Wordpress plugin that shows the last videos from a Youtube channel choosen by the site administrator. It's possible to include it as a widget in the home page or in the middle of a post with a list view or a grid view.
--- src/my-youtube-recommendation.php
+++ src/my-youtube-recommendation.php
@@ -1,11 +1,62 @@
 <?php
 
     /**
-     * Plugin Name: My Youtube Recommendation
+     * @link                https://github.com/andredss99/my-youtube-recommendation
+     * @since               1.0.0
+     * @package             My_Youtube_Recommendation
+     * 
+     * @wordpress-plugin
+     * Plugin Name:         My Youtube Recommendation
+     * Plugin URI:          https://github.com/andredss99/my-youtube-recommendation
+     * Description:         Display the last videos from a Youtube channel using Youtube feed.
+     * Version:             1.0.0
+     * Author:              André Sousa
+     * Author URI:          https://github.com/andredss99
+     * License:             GPLv3 or later
+     * License URI:         https://www.gnu.org/licenses/gpl-3.0.html
+     * Text Domain:         my-youtube-recommendation
+     * Domain Path:         /languages/
      */
 
-    add_filter('the_content', 'thanks');
+    if (!defined('WPIND')) {
+        wp_die();
+    }
+    
+    // Plugin Version
+    if (!defined('MY_YOUTUBE_RECOMMENDATION_VERSION')) {
+        define('MY_YOUTUBE_RECOMMENDATION_VERSION', '1.0.0');
+    }
 
-    function thanks($content) {
-        return $content . "<p><strong></strong></p>";
+    // Plugin Name
+    if (!defined('MY_YOUTUBE_RECOMMENDATION_NAME')) {
+        define('MY_YOUTUBE_RECOMMENDATION_NAME', 'My Youtube Recommendation');
+    }
+
+    // Plugin Slug
+    if (!defined('MY_YOUTUBE_RECOMMENDATION_PLUGIN_SLUG')) {
+        define('MY_YOUTUBE_RECOMMENDATION_PLUGIN_SLUG', 'my-youtube-recommendation');
+    }
+
+    // Plugin Basename
+    if (!defined('MY_YOUTUBE_RECOMMENDATION_BASENAME')) {
+        define('MY_YOUTUBE_RECOMMENDATION_BASENAME', plugin_basename(__FILE__));
+    }
+
+    // Plugin Folder
+    if (!defined('MY_YOUTUBE_RECOMMENDATION_PLUGIN_DIR')) {
+        define('MY_YOUTUBE_RECOMMENDATION_PLUGIN_DIR', plugin_dir_path(__FILE__));
+    }
+
+    // JSON File Name
+    if (!defined('MY_YOUTUBE_RECOMMENDATION_JSON_FILENAME')) {
+        define('MY_YOUTUBE_RECOMMENDATION_JSON_FILENAME', 'my-yt-rec.json');
+    }
+
+    require_once MY_YOUTUBE_RECOMMENDATION_PLUGIN_DIR . "includes/class-youtube-recommendation.php";
+    require_once MY_YOUTUBE_RECOMMENDATION_PLUGIN_DIR . "includes/class-youtube-recommendation-json.php";
+    require_once MY_YOUTUBE_RECOMMENDATION_PLUGIN_DIR . "includes/class-youtube-recommendation-shortcode.php";
+    require_once MY_YOUTUBE_RECOMMENDATION_PLUGIN_DIR . "includes/class-youtube-recommendation-widget.php";
+
+    if (is_admin()) {
+        require_once MY_YOUTUBE_RECOMMENDATION_PLUGIN_DIR . "includes/class-youtube-recommendation-admin.php";
     }
